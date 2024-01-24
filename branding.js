0c6455cd384bd1ab794e7a8a7cd68a5e07db4814
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-01-24 10:52:23 -0800

    Add files via upload

---
+++ branding.js
@@ -0,0 +1,5 @@
+// This file sideloads CSS information
+
+$( document ).ready(function() {
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://confluence.douglascollege.ca:2161/tdx/branding.css">');
+});
