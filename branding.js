4655b5896d0195c52532debd75a157ebbd29d06b
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-01-24 15:16:15 -0800

    Update branding.js

--- branding.js
+++ branding.js
@@ -1,5 +1,5 @@
 // This file sideloads CSS information
 
 $( document ).ready(function() {
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://confluence.douglascollege.ca:2161/tdx/branding.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/Kierkegaard2024/TDX/branding.css">');
 });
