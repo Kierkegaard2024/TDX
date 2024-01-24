602fe04dd38336a62026e8ded0ad4d70539e4df5
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-01-24 15:06:08 -0800

    Add files via upload

---
+++ terminology.js
@@ -0,0 +1,6 @@
+window.onload = clear();
+
+function clear() {            
+	document.body.innerHTML = document.body.replace('Service Catalog', 'Services');
+	document.body.innerHTML = document.body.replace('Knowledge Base', 'Information');
+} // JavaScript Document
