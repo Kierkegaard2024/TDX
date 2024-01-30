5b410a8a45cef9f5f85ea87f814ebb2b6c6a8b3f
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-01-30 15:12:46 -0800

    Create theme-colour-341.js

---
+++ theme-colour-341.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+	$('head').append('<meta name="theme-color" media="(prefers-color-scheme: light)" content="#0077b6" /><meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0077b6" />');
+});
