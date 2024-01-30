062160520dbfc68dd3d40ce9e8c521e1c1f906d5
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-01-30 15:12:05 -0800

    Create theme-colour.js

---
+++ theme-colour.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+	$('head').append('<meta name="theme-color" media="(prefers-color-scheme: light)" content="#93AA24" /><meta name="theme-color" media="(prefers-color-scheme: dark)" content="#93AA24" />');
+});
