73994765324baf37f03632950ce51f96fafc3d31
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-02-26 11:53:15 -0800

    Create googlefonts.js

---
+++ stable/googlefonts.js
@@ -0,0 +1,11 @@
+// This file loads Google Font information for Montserrat and Roboto Condensed
+
+$( document ).ready(function() {
+	$('head').append('<link rel="preconnect" href="https://fonts.googleapis.com">');
+});
+$( document ).ready(function() {
+	$('head').append('<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>');
+});
+$( document ).ready(function() {
+	$('head').append('<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Roboto+Condensed:wght@700;800&display=swap" rel="stylesheet">');
+});
