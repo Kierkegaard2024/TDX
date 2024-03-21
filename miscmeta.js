0229412bb1115650cbd3b02c6ef6515f1789b5af
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-03-21 11:57:43 -0700

    Create miscmeta.js

---
+++ miscmeta.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+	$('head').append('<link rel="apple-touch-icon" sizes="96x96" href="https://cdn.jsdelivr.net/gh/Kierkegaard2024/TDX/favicon96.png" />');
+  $('head').append('<meta name="description" content="Explore Douglas College\'s Solutions website for helpful articles, guides and tutorials on technology, software and IT services. Find answers to common questions and learn how to troubleshoot issues." />');
+});
