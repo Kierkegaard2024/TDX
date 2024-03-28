df98de3cd43bc59cefbcb08d5d1a318663b220e5
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2024-03-28 11:47:16 -0700

    Update and rename searchbarplaceholder-v2.js to searchbarplaceholder-v3.js

--- searchbarplaceholder-v2.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("SiteSearch-text-43841252b4c84381a2a1335ba208f1f7").setAttribute("placeholder", "Search this site");
-});
---
+++ searchbarplaceholder-v3.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".form-control").setAttribute("placeholder", "Search this site");
+});
