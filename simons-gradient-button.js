bf493c920efb2b2506442beeb0b743bd99430235
Kierkegaard2024 <157637891+Kierkegaard2024@users.noreply.github.com>
2025-04-09 15:08:03 -0700

    Add files via upload

---
+++ simons-gradient-button.js
@@ -0,0 +1,5 @@
+// This file sideloads CSS information
+
+$( document ).ready(function() {
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/Kierkegaard2024/TDX/simons-gradient-button.css">');
+});
